724b510f7988f7b39b14bcf57688082759e59148
yan li <yanli@yandeMBP.lan>
2020-03-27 17:33:46 +0800

    update settings

---
+++ __init__.py
@@ -0,0 +1,57 @@
+
+from flask import Flask
+from bluelog.settings import config
+from bluelog.extensions import bootstrap,db,moment,cheditor,mail
+
+
+def create_app(config_name=None):
+	if config_name  is None:
+		config_name = os.getenv('FLASK_CONFIG','development')
+
+	app = Flask('bluelog')
+	app.config.from_object(config[config_name])
+
+	register_logging(app)
+	register_extensions(app)
+	register_blueprints(app)
+	register_commands(app)
+	register_errors(app)
+	register_shell_context(app)
+	register_template_context(app)
+	return app
+
+def register_logging(app):
+	pass
+
+def register_extensions(app):
+	bootstrap.init_app(app)
+	db.init_app(app)
+	cheditor.init_app(app)
+	mail.init_app(app)
+	moment.init_app(app)
+
+def register_blueprints(app):
+	app.register_blueprint(blog_bp)
+	app.register_blueprint(admin_bp,url_prefix='/admin')
+	app.register_blueprint(auth_bp,url_prefix='/auth')
+
+def register_shell_context(app):
+	@app.shell_context_processor
+	def make_shell_context():
+		return dict(db=db)
+
+def register_errors(app):
+	@app.errorhandler(400)
+	def bad_request(e):
+		return render_template("errors/400.html"),400
+
+
+def reigster_commands(app):
+	pass
+
+
+
+
+
+
+
---
+++ extensions.py
@@ -0,0 +1,13 @@
+
+
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy 
+from flask_mail import Mail 
+from flask_ckeditor import CKEditor
+from flask_moment import Moment
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+moment = Moment()
+cheditor = CKEditor()
+mail = Mail()
